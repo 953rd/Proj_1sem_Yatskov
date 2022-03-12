2b76168ed71bb81c0a26aa550269ad96baf28c17
Creon228 <yatskovi@bk.ru>
2022-03-12 10:43:03 +0300

    In PZ_13_1 & PZ_13_2 was added condition

--- PZ_13/PZ_13_2.py
+++ PZ_13/PZ_13_2.py
@@ -1,3 +1,8 @@
+# Из заданной строки отобразить только символы нижнего регистра.
+# Использовать библиотеку string. Строка 'In PyCharm,
+# you can specify third-party standalone applications and run
+# them as External Tools'
+
 import string
 
 fs = 'In PyCharm, you can specify third-party standalone' \
