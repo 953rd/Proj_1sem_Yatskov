20ea36ee95f4eeedacfcb4781c7057883f55bc82
Creon228 <yatskovi@bk.ru>
2022-03-12 10:34:22 +0300

    PZ_13 was added

---
+++ PZ_13/PZ_13_2.py
@@ -0,0 +1,11 @@
+import string
+
+fs = 'In PyCharm, you can specify third-party standalone' \
+     ' applications and run them as External Tools'
+ps = list()
+for i in fs:
+    for d in string.ascii_lowercase:
+        if i == d:
+            ps.append(i)
+print(fs)
+print(ps)
