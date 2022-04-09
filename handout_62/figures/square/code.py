074ad56adc5f7c7eee11349100a57d64f6cf7317
Creon228 <yatskovi@bk.ru>
2022-04-09 11:30:01 +0300

    handout_62 was edited

--- handout_62/figures/square/code.py
+++ handout_62/figures/square/code.py
@@ -1,5 +1,4 @@
-import math
-def triangle_perimetr():
+def square_perimetr():
     print('1: Ввести свои стороны. 2: Использовать стандартные стороны.')
     n = int(input('Выберите число 1 или 2: '))
     while (n != 1) and (n != 2):
@@ -10,17 +9,13 @@ def triangle_perimetr():
             pass
     if n == 1:
         a = int(input('Введите сторну a: '))
-        b = int(input('Введите сторну b: '))
-        c = int(input('Введите сторну c: '))
     else:
         a = d_a
-        b = d_b
-        c = d_c
-    p = a + b + c
-    print('Периметр треугольника = ', p)
+    p = a*4
+    print('Периметр квадрата = ', p)
 
 
-def triangle_area():
+def square_area():
     print('1: Ввести свои стороны. 2: Использовать стандартные стороны.')
     n = int(input('Выберите число 1 или 2: '))
     while (n != 1) and (n != 2):
@@ -31,17 +26,10 @@ def triangle_area():
             pass
     if n == 1:
         a = int(input('Введите сторну a: '))
-        b = int(input('Введите сторну b: '))
-        c = int(input('Введите сторну c: '))
     else:
         a = d_a
-        b = d_b
-        c = d_c
-    pp = (a + b + c) / 2
-    s = math.sqrt(pp*(pp-a)*(pp-b)*(pp-c))
-    print("Площадь теугольника = ", s)
+    s = a**2
+    print("Площадь квадрата = ", s)
 
 
-d_a = 7
-d_b = 2
-d_c = 8
+d_a = 15
