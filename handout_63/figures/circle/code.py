a531e9e4c477ddc92039ac034c27fb82da25c052
Teo <64589970+953rd@users.noreply.github.com>
2022-04-12 22:35:36 +0300

    Update code.py

--- handout_63/figures/circle/code.py
+++ handout_63/figures/circle/code.py
@@ -1,37 +1,22 @@
-def circle_perimetr():
-    print('1: Ввести свой радиус. 2: Использовать стандартный радиус.')
-    n = int(input('Выберите число 1 или 2: '))
-    while (n != 1) and (n != 2):
+def circle_perimetr(d_r=5):
+    a = input("Введите радиус окружности: ")
+    while type(a) != int:
         try:
-            print('Такого варианта нет')
-            n = int(input('Выберите число 1 или 2: '))
+            a = int(a)
         except ValueError:
-            pass
-    if n == 1:
-        r = int(input('Введите радиус: '))
-    else:
-        r = default_radius
+            a = d_r
     pi = 3.14
-    p = 2*pi*r
+    p = 2*pi*a
     print('Длина окружности = ', p)
 
 
-def circle_area():
-    print('1: Ввести свой радиус. 2: Использовать стандартный радиус.')
-    n = int(input('Выберите число 1 или 2: '))
-    while (n != 1) and (n != 2):
+def circle_area(d_r=5):
+    a = input("Введите радиус окружности: ")
+    while type(a) != int:
         try:
-            print('Такого варианта нет')
-            n = int(input('Выберите число 1 или 2: '))
+            a = int(a)
         except ValueError:
-            pass
-    if n == 1:
-        r = int(input('Введите радиус: '))
-    else:
-        r = default_radius
+            a = d_r
     pi = 3.14
-    s = pi*r**2
+    s = pi*a**2
     print("Площадь окружности: ", s)
-
-
-default_radius = 5
