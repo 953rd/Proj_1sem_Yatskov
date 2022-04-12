de80f92827bece0d238b295dd0773833f82fdab7
Teo <64589970+953rd@users.noreply.github.com>
2022-04-12 22:36:28 +0300

    Update code.py

--- handout_63/figures/square/code.py
+++ handout_63/figures/square/code.py
@@ -1,35 +1,21 @@
-def square_perimetr():
-    print('1: Ввести свои стороны. 2: Использовать стандартные стороны.')
-    n = int(input('Выберите число 1 или 2: '))
-    while (n != 1) and (n != 2):
+def square_perimetr(d_a=15):
+    a = input("Введите сторону квадрата: ")
+    while type(a) != int:
         try:
-            print('Такого варианта нет')
-            n = int(input('Выберите число 1 или 2: '))
+            a = int(a)
         except ValueError:
-            pass
-    if n == 1:
-        a = int(input('Введите сторну a: '))
-    else:
-        a = d_a
+            a = d_a
     p = a*4
     print('Периметр квадрата = ', p)
 
 
-def square_area():
-    print('1: Ввести свои стороны. 2: Использовать стандартные стороны.')
-    n = int(input('Выберите число 1 или 2: '))
-    while (n != 1) and (n != 2):
+def square_area(d_a=15):
+    a = input("Введите сторону квадрата: ")
+    while type(a) != int:
         try:
-            print('Такого варианта нет')
-            n = int(input('Выберите число 1 или 2: '))
+            a = int(a)
         except ValueError:
-            pass
-    if n == 1:
-        a = int(input('Введите сторну a: '))
-    else:
-        a = d_a
+            a = d_a
     s = a**2
     print("Площадь квадрата = ", s)
-
-
-d_a = 15
+    
